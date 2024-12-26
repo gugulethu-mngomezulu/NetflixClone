10b8c28f2475ef8fbe77c27feaa3982fe4865da0
Gugulethu Mngomezulu <56169293+gugulethu-mngomezulu@users.noreply.github.com>
2024-12-26 17:45:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add interactivity to the email form
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you for signing up! Stay tuned for updates.");
+  });
+  
